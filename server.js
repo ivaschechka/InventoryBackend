a0689387a780f5c19ab0bc57f3a79a54a218f482
Ivan Antonov <ivan_antonov@live.ru>
2017-09-06 22:02:02 +0300

    edit cors

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ Object.assign = require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
+app.use(function(req, res, next) {
+    var origins = [
+        '*'
+    ];
 
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
