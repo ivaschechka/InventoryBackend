ee01c7b7db9f2df2dbbb5b3feb4f60c4c38546a9
Ivan Antonov <ivan_antonov@live.ru>
2017-09-09 18:36:46 +0300

    add module db

---
+++ db.js
@@ -0,0 +1,23 @@
+var mongodb = require('mongodb');
+
+var state = {
+    db: null
+};
+
+exports.connect = function(url, done) {
+    if (state.db) {
+        return done();
+    }
+    mongodb.connect(url, function(err, db) {
+        if (err) {
+            return done(err);
+            return;
+        }
+        state.db = db;
+        done();
+    })
+}
+
+exports.get = function() {
+    return state.db;
+}
