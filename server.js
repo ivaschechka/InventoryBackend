23d39f1350324e7d05f63a93d690e9c61423b56d
Ivan Antonov <ivan_antonov@live.ru>
2017-09-11 18:13:52 +0300

    edit bodyparser

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ var express = require('express'),
     objectId = require('mongodb').ObjectID;
 var categoriesController = require('./controllers/categories');
 Object.assign = require('object-assign')
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
+var jsonParser = bodyParser.json()
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 app.use(cors());
@@ -111,7 +111,7 @@ app.get('/', function(req, res,  next) {
 
 app.get('/categories', categoriesController.all); // Просмотр всех категорий
 app.get('/categories/:id', categoriesController.findById); // Просмотр категории id
-app.post('/categories', categoriesController.create); // Добавление новой категории
+app.post('/categories', jsonParser, categoriesController.create); // Добавление новой категории
 app.put('/categories/:id', categoriesController.update); // Обновление категории id
 app.delete('/categories/:id', categoriesController.delete); // Удаление категории id
 
