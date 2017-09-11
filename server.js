bba04f8d73159749e8eb113aea5bb754817579e2
Ivan Antonov <ivan_antonov@live.ru>
2017-09-11 21:07:34 +0300

    test

--- server.js
+++ server.js
@@ -17,21 +17,22 @@ app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 app.use(cors());
 app.use(function(req, res, next) {
+    console.log(req.method);
     if (req.method === 'OPTIONS') {
         console.log('!OPTIONS');
         var headers = {};
         // IE8 does not allow domains to be specified, just the *
         // headers["Access-Control-Allow-Origin"] = req.headers.origin;
         headers["Access-Control-Allow-Origin"] = "*";
-        headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+        headers["Access-Control-Allow-Methods"] = "OPTIONS";
         headers["Access-Control-Allow-Credentials"] = false;
         headers["Access-Control-Max-Age"] = '86400'; // 24 hours
         headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
         res.writeHead(200, headers);
-        res.end();
+        next();
     } else {
         res.header('Access-Control-Allow-Origin', req.headers.origin);
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     }
