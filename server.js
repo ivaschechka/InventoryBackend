cd99554d7e3598ced21640cbc8ade0cb005b32fc
Ivan Antonov <ivan_antonov@live.ru>
2017-09-11 22:37:48 +0300

    bb1

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
         next();
     }
 });
-app.use(cors());
+// app.use(cors());
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
