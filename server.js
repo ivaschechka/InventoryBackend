6c93db47236adc6755cf488078b34b237c1188fc
Ivan Antonov <ivan_antonov@live.ru>
2017-09-13 22:49:56 +0300

    edit port

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json())
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8070,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
